9a34cccf8c7041a45a2dfccb1101d51524036e54
kobz2414 <62010035+kobz2414@users.noreply.github.com>
2021-03-13 22:38:09 +0800

    Clean

--- js/app.js
+++
@@ -1,3 +0,0 @@
-function youtubeLink(){
-  document.getElementById("temp").innerHTML += "<br><br><center><iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/KIgjsRTgbvA?rel=0?version=3&autoplay=1&controls=0&&showinfo=0\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe><center>";
-}
